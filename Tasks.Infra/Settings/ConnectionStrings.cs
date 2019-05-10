5562ed4e94bafa6f650748a50e336d6147a01b71
Alan Zelaquett Hoffmann <alanzhoffmann@gmail.com>
2019-05-10 01:17:19 -0300

    Início de implementação de estrutura da aplicação

---
+++ Tasks.Infra/Settings/ConnectionStrings.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Tasks.Infra
+{
+    public sealed class ConnectionStrings
+    {
+        public ConnectionStrings(IConfiguration configuration)
+        {
+            Tasks = configuration.GetConnectionString("Tasks");
+        }
+
+        public string Tasks { get; }
+    }
+}
